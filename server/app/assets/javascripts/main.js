79e3114790b3d53e8fe15cc03ae524f7592d48c6
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-11-28 12:11:49 +0100

    dispatch handlers

--- server/app/assets/javascripts/main.js
+++ server/app/assets/javascripts/main.js
@@ -1,17 +1,38 @@
-var source = new EventSource("/stream");
 
-source.addEventListener('message', function(e) {
-  var json = JSON.parse(e.data);
-  
-}, false);
-
-source.addEventListener('open', function(e) {
-  // Connection was opened.
-}, false);
-
-source.addEventListener('error', function(e) {
-  if (e.readyState == EventSource.CLOSED) {
-    // Connection was closed.
+var sensors = {
+  "light": function (state) {
+    console.log(state ? "light on" : "light off");
+  },
+  "humidity": function (value) {
+    console.log("humidity = " + value);
+  },
+  "temperature": function (value) {
+    console.log("temperature = " + value);
   }
-}, false);
+};
 
+
+////////// Connect and handle the Stream ///////
+
+function connect () {
+  var source = new EventSource("/stream");
+
+  source.addEventListener('message', function(e) {
+    var json = JSON.parse(e.data);
+    for (var key in json)
+      if (key in sensors)
+        sensors[key](json[key]);
+  }, false);
+
+  source.addEventListener('open', function(e) {
+    // Connection was opened.
+  }, false);
+
+  source.addEventListener('error', function(e) {
+    if (e.readyState == EventSource.CLOSED) {
+      // Connection was closed.
+    }
+  }, false);
+}
+
+connect();
