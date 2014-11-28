6b36b9be4ef81340210c93ca460f0a6d3954cba3
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-11-28 11:35:54 +0100

    add routes and some assets

---
+++ server/app/assets/javascripts/main.js
@@ -0,0 +1,15 @@
+var source = new EventSource("/stream");
+
+source.addEventListener('message', function(e) {
+  console.log(e.data);
+}, false);
+
+source.addEventListener('open', function(e) {
+  // Connection was opened.
+}, false);
+
+source.addEventListener('error', function(e) {
+  if (e.readyState == EventSource.CLOSED) {
+    // Connection was closed.
+  }
+}, false);
--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -9,4 +9,12 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def light = Action {
+    NotImplemented
+  }
+
+  def stream = Action {
+    NotImplemented
+  }
+
 }
