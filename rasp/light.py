70b39a60aeef29e237332394b0dc2c977a616ca1
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-11-28 13:01:09 +0100

    python light code

---
+++ rasp/light.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+import time
+import sys
+
+url='http://10.0.25.240:8080/light/'
+nodata = "".encode("UTF-8")
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4,GPIO.IN)
+
+while True:
+    try:
+        value=GPIO.input(4)
+        urlopen(url + value, data=nodata, timeout=1)
+        print(value, end="", flush=True)
+    except Exception as e:
+        print('x', end="", flush=True)
+    time.sleep(1)
